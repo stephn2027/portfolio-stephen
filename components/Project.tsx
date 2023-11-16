dca4f58aac487e879a7d26ec065a12268bc6ea56
stephen <stephn2027@gmail.com>
2023-11-16 20:22:47 +0900

    added projects component and project effects

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import { projectsData } from '@/lib/data';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import Image from 'next/image';
-import React from 'react';
+import React, { useRef } from 'react';
 
 type ProjectProps = (typeof projectsData)[number];
 
@@ -10,9 +13,18 @@ export default function Project({
   tags,
   imageUrl,
 }: ProjectProps) {
+    const ref  = useRef<HTMLDivElement>(null);
+    const {scrollYProgress} = useScroll({
+        target:ref,
+        offset:['0 1', '1.33 1'],
+    });
+    const scaleProgress = useTransform(scrollYProgress,[0,1],[0.75,1])
+    const opacityProgress = useTransform(scrollYProgress,[0,1],[0.6,1])
+
   return (
-  <section className='bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]'>
-    <div className='py-4 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full'>
+    <motion.div ref={ref} style={{scale:scaleProgress, opacity:opacityProgress}} className='mb-3 sm:mb-8 last:mb-0 group'>
+  <section className='group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] group-even:pl-8 hover:bg-gray-200 transition rounded-lg'>
+    <div className='pt-4 pb-8 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]'>
 
     <h3 className='text-2xl font-semibold'>{title}</h3>
     <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
@@ -22,7 +34,8 @@ export default function Project({
     ))}
     </ul>
     </div>
-    <Image src={imageUrl} alt={title} quality={95} className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl'/>
+    <Image src={imageUrl} alt={title} quality={95} className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 group-even:right-[initial] group-even:-left-40'/>
     </section>
+    </motion.div>
   )
 }
