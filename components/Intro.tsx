4b6f156316493324f2508c2cf1c2e1de0baa22bf
stephen <stephn2027@gmail.com>
2023-12-21 13:14:11 +0900

    fixed responsive mobile design

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -4,7 +4,7 @@
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
-import React, from 'react';
+import React from 'react';
 import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
 import { FaGithubSquare } from 'react-icons/fa';
 
