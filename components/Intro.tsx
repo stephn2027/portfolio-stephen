5181b51b5a4bdb569baa775aa6ca298d08e6a47f
stephen <stephn2027@gmail.com>
2024-03-26 14:16:29 +0900

    configured download link of CV

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -85,7 +85,7 @@ export default function Intro() {
         <a
           className="group bg-white px-7 py-3 gap-2 flex items-center rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href="/StephenUy2024.pdf"
-          download
+          download={true}
         >
           Download CV{' '}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
